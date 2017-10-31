93cb991dd6bad54f7de1718da771003326688a81
Emilie Dunphy <emilie.dunphy@gmail.com>
2017-10-31 18:21:28 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -17,8 +17,8 @@ function doWhileLoop(array) {
   function maybeTrue() {
     return Math.random() >= 0.5
   }
-   
+
   do {
-    console.log('doo-bee-doo-bee-doo')
-  } while (maybeTrue());
+    {array=array.splice}
+  } while (array.length > 0 && maybeTrue());
 }
