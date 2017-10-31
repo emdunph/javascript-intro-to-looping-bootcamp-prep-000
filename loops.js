a7844c871416344a82c022922fce06d741d86e9e
Emilie Dunphy <emilie.dunphy@gmail.com>
2017-10-31 17:13:14 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,9 +1,10 @@
 function forLoop(array){
   for (let i = 0; i < 25; i++)
-  if {(i ===1)}
-  if {array[i] = "I am 1 strange loop."}
-  else
-   {array[i] = `I am ${i} strange loops.`}
+  if {(i === 1) 
+  var array = [`I am 1 strange loop.`]}
+  else {(i === 2)}
+  var array = [`I am ${i} strange loops.`]
+  
 }
 return array;
 }
@@ -11,3 +12,7 @@ return array;
 function whileLoop(n){
 
 }
+if {(i ===1)}
+if {array[i] = "I am 1 strange loop."}
+else
+ {array[i] = `I am ${i} strange loops.`}
