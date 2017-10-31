7820942a6cbf89ba3a1b4f2a9f417e15f55e9ed9
Emilie Dunphy <emilie.dunphy@gmail.com>
2017-10-31 17:28:47 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,10 +1,17 @@
-function forLoop(array){
-  for (let i = 0; i < 25; i++);
-  if {(i === 1);
-  var array = ["I am 1 strange loop."]};
-  else {(i === 2)};
-  var array = [`I am ${i} strange loops.`];
-return array;
+function forLoop (array) {
+  function forLoop(array) {
+  for (let i = 0; i < 25; i++) {
+    if (i === 1) {
+      array.push(`"I am 1 strange loop."`);
+    }else{
+      array.push(`"I am ${i} strange loop${i === 0 ? '' : 's'}."`);
+    }
+
+  }
+
+  return array;
+}
+  }
 }
 
 }
