f2a1f53eb8605457e575cc15151cea10506d16aa
Emilie Dunphy <emilie.dunphy@gmail.com>
2017-10-31 17:14:04 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,7 +1,7 @@
 function forLoop(array){
   for (let i = 0; i < 25; i++)
   if {(i === 1)
-  var array = [`I am 1 strange loop.`]}
+  var array = ["I am 1 strange loop."]}
   else {(i === 2)}
   var array = [`I am ${i} strange loops.`]
 
